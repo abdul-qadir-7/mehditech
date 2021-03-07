3af328643411971f7e2f4189ccb0288781fae7ab
abdul-qadir-7 <54612502+abdul-qadir-7@users.noreply.github.com>
2021-03-07 14:52:04 +0530

    mehditech first version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+ anchorScrolling: 'enabled',
+  //scrollOffset: [10, 4],
+};
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
